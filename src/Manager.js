08e6f1b82f045bc35c7f83a78c03f1e188d7eee9
SebestikCZ <44745014+SebestikCZ@users.noreply.github.com>
2020-11-19 15:56:32 +0100

    Update Manager.js

--- src/Manager.js
+++ src/Manager.js
@@ -1,6 +1,9 @@
 const db = require('mariadb');
 
-get: async function (options, table, key, value) {
-	mariadb.createPool(options);
-	var conn = 
+get: async function (options, table, key) {
+	const pool = mariadb.createPool(options);
+	var conn = pool.getConnection();
+	var sql = `SELECT ${key} from ${table}`;
+	let result;
+	conn.query(sql).then(result = {});
 }
