98656aa56359f6ef2e31975d2b31c53b7c852f45
SebestikCZ <44745014+SebestikCZ@users.noreply.github.com>
2020-11-20 12:37:46 +0100

    Update Manager.js

--- src/Manager.js
+++ src/Manager.js
@@ -2,12 +2,24 @@ const db = require('mariadb');
 const get = require('./get.js');
 const set = require('./set.js');
 
-get: async function (options, table, key) {
-	const pool = mariadb.createPool(options);
-	var conn = pool.getConnection();
-	var sql = `SELECT ${key} from ${table}`;
-	let result;
-	conn.query(sql).then(result = {});
-	const gettedValue = get.getValue(result);
-	return gettedValue;
+module.exports = {
+	get: async function (options, table, key) {
+		const pool = mariadb.createPool(options);
+		var conn = pool.getConnection();
+		var sql = `SELECT ${key} from ${table}`;
+		let result;
+		conn.query(sql).then(result = {});
+		const gettedValue = get.getValue(result);
+		return gettedValue;
+	},
+	set: async function (options, table, key, value) {
+		const pool = mariadb.createPool(options);
+		var conn = pool.getConnection();
+		var selection = conn.query(`SELECT ${key} from ${table}`);
+		var sql;
+		if (selection.includes(value)) {
+			sql = `UPDATE \`${table}\` SET WHERE ${selection.indexof(value)}`
+		}
+		sql = `INSERT ${key} INTO ${table}`;
+	}
 }
