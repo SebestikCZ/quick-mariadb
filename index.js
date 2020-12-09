16a8d048778b5eb132cfeb0ad9dd8e314152251d
SebestikCZ <44745014+SebestikCZ@users.noreply.github.com>
2020-12-09 19:04:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const Manager = require(./src/Manager.js);
 
 module.exports = {
 	get: async function(config, table, key) {
-		Manager.get(config, table, key);
+		return Manager.get(config, table, key);
 	},
 	set: async function(config, table, key, value) {
-		Manager.set(config, table, key, value);
+		return Manager.set(config, table, key, value);
 	}
 }
